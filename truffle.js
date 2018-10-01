3eed25de0088716801637e2e2a92b0ce670dc04a
amitbdj1481 <amitbdj1481@gmail.com>
2018-10-01 18:50:16 +0400

    Added files for Recordskeeper configurations

--- truffle.js
+++ truffle.js
@@ -1,6 +1,14 @@
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
   networks: {
     development: {
       host: "127.0.0.1",
@@ -8,10 +16,11 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     recordskeeper:{
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://35.170.155.89:8378/");
-      },
-      network_id: '*',
-    }
+      host: "35.172.1.247",
+      port: 7895,
+      network_id: '1234',
+      from:'121TA9XY5U5RfLrfdaMHe8d7es7MmmktBasjCH'
+    },
+    
   }
 };
